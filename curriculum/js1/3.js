8fe11a14f285a168d96aeb758e5900b13a678cac
Tommy Souksamlane <tommy_s@apple.com>
2018-03-24 15:57:33 -0700

    thx jeffrey and madhu for their help today

--- curriculum/js1/3.js
+++ curriculum/js1/3.js
@@ -5,9 +5,14 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b="hello", result="") => {
+    if (a === 0) { return result }
+    result += b;
+    a -= 1;
+    return solution(a, b, result);
 };
+
+console.log(solution(3, 'hello'));
 
 module.exports = {
   solution,
