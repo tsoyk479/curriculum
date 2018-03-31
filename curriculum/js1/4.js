f5aab3c9a11bcaf7e6adb42ae08637373e26df19
Tommy Souksamlane <tommy_s@apple.com>
2018-03-31 15:14:11 -0700

    Please review js1 / 4.js exercise

--- curriculum/js1/4.js
+++ curriculum/js1/4.js
@@ -6,8 +6,11 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, result="") => {
+  if (a === 0) return result;
+  result += b;
+  a -= 1;
+  return solution(a, b, result);
 };
 
 module.exports = {
