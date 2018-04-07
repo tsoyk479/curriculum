6764d13a47c3a39c0f18e7398788ae313746acad
Tommy Souksamlane <tommy_s@apple.com>
2018-04-07 15:24:30 -0700

    remove ide file

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -5,8 +5,15 @@
  * @returns {boolean}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, b=1, cnt=0) => {
+  // primes are greater than 1 whole numbers
+  if (a === 1) return false;
+  // increment divisible count
+  if (a != b && a % b === 0) ++cnt;
+  // a prime is a number only be divisible by itself
+  if (b === a) return cnt === 1;
+
+  return solution(a, b+1, cnt);
 };
 
 module.exports = {
